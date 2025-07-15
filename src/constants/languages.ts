1e6fa5b800fc0833d814df42a58de6094f732b79
lionel.wilson <lwilson@cynergybank.co.uk>
2025-07-15 21:30:13 +0100

    feat: Add canto, dutch, tagalog and yoruba as languages

--- src/constants/languages.ts
+++ src/constants/languages.ts
@@ -2,15 +2,19 @@ import { Language } from '../types';
 
 export const LANGUAGES: Language[] = [
   { code: 'English', name: 'English', flag: '🇺🇸' },
+  { code: 'Japanese', name: 'Japanese', flag: '🇯🇵' },
+  { code: 'Korean', name: 'Korean', flag: '🇰🇷' },
   { code: 'Spanish', name: 'Spanish', flag: '🇪🇸' },
   { code: 'French', name: 'French', flag: '🇫🇷' },
+  { code: 'Dutch', name: 'Dutch', flag: '🇳🇱' },
   { code: 'German', name: 'German', flag: '🇩🇪' },
   { code: 'Italian', name: 'Italian', flag: '🇮🇹' },
   { code: 'Portuguese', name: 'Portuguese', flag: '🇵🇹' },
+  { code: 'Yoruba', name: 'Yoruba', flag: '🇳🇬' },
   { code: 'Russian', name: 'Russian', flag: '🇷🇺' },
-  { code: 'Japanese', name: 'Japanese', flag: '🇯🇵' },
-  { code: 'Korean', name: 'Korean', flag: '🇰🇷' },
   { code: 'Chinese', name: 'Chinese', flag: '🇨🇳' },
+  { code: 'Cantonese', name: 'Cantonese', flag: '🇭🇰' },
+  { code: 'Tagalog', name: 'Tagalog', flag: '🇵🇭' },
   { code: 'Arabic', name: 'Arabic', flag: '🇸🇦' },
   { code: 'Hindi', name: 'Hindi', flag: '🇮🇳' },
 ];
